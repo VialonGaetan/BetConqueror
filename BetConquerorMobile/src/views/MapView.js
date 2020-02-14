2a8b51ad93fdaf135f0bfcb2a272304f7aeb7945
Gaetan Vialon <gaetan.vialon@gmail.com>
2020-02-14 08:41:05 +0100

    Gros Clean code

--- BetConquerorMobile/src/views/MapView.js
+++ BetConquerorMobile/src/views/MapView.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ZoneComponent from '../components/zone';
-import {View, StyleSheet, Dimensions, Text} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import { View, StyleSheet, Dimensions, Text, TouchableOpacity } from 'react-native';
 import War from '../models/war';
 import Zone from '../models/zone';
 
@@ -14,29 +13,29 @@ import Zone5 from '../../assets/Island5.png';
 const MapView = props => {
   const war = new War(
     '1',
-    [{username: 'player 1'}, {username: 'player2'}],
+    [{ username: 'player 1' }, { username: 'player2' }],
     'nom de la zone',
   );
   war.tour = 1;
-  war.winner = {username: 'player 1'};
+  war.winner = { username: 'player 1' };
   war.winningBet = 10;
 
   const war2 = new War(
     '2',
-    [{username: 'player 3'}, {username: 'player4'}],
+    [{ username: 'player 3' }, { username: 'player4' }],
     'nom de la zone',
   );
   war2.tour = 1;
-  war2.winner = {username: 'player 1'};
+  war2.winner = { username: 'player 1' };
   war2.winningBet = 5;
   const wars = [war, war2];
   const onZonePressed = zoneID => {
     props.navigation.navigate('ZoneHistory', {
-      zone: {id: zoneID, name: 'nom de la zone', wars},
+      zone: { id: zoneID, name: 'nom de la zone', wars },
     });
   };
   return (
-    <View style={{flex: 1, backgroundColor: 'lightblue'}}>
+    <View style={{ flex: 1, backgroundColor: 'lightblue' }}>
       <View
         style={{
           flex: 1,
@@ -44,7 +43,7 @@ const MapView = props => {
           justifyContent: 'space-around',
           top: 50,
         }}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
           <TouchableOpacity onPress={() => onZonePressed(1)}>
             <ZoneComponent imageSource={Zone4} zoneID={0} />
           </TouchableOpacity>
@@ -52,12 +51,12 @@ const MapView = props => {
             <ZoneComponent imageSource={Zone3} zoneID={2} />
           </TouchableOpacity>
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
           <TouchableOpacity onPress={() => onZonePressed(3)}>
             <ZoneComponent imageSource={Zone1} zoneID={4} />
           </TouchableOpacity>
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
           <TouchableOpacity onPress={() => onZonePressed(4)}>
             <ZoneComponent imageSource={Zone2} zoneID={1} />
           </TouchableOpacity>
--- TUIOBetConqueror/src/index.js
+++ TUIOBetConqueror/src/index.js
@@ -7,173 +7,26 @@ import "@lottiefiles/lottie-player";
 import TUIOManager from "tuiomanager/core/TUIOManager";
 import QrCode from "qrcode";
 import SocketClient from "./SocketClient/SocketClient";
-import buildBoard from "./Board";
-import { getRaceValue } from "./Race";
-import GameInstance from "./models/GameInstance";
+import mySocketHandler from "./SocketClient/SocketHandler";
+import speak from './Speaker'
 
 /* TUIOManager start */
 const tuioManager = new TUIOManager();
 tuioManager.start();
 
 
-//const SERVER_ADRESS = "ws://10.212.120.221:8080/game";
-const SERVER_ADRESS = "ws://192.168.1.3:8080/game";
+const SERVER_ADRESS = "ws://10.212.120.221:8080/game";
+//const SERVER_ADRESS = "ws://192.168.1.3:8080/game";
 
 const socketClient = new SocketClient();
 socketClient.start(SERVER_ADRESS);
 
-
-const synth = window.speechSynthesis;
-
-const audio = new Audio();
+export const audio = new Audio();
 audio.volume = 0.4;
 audio.loop = true;
 
-const gameInstance = new GameInstance();
 
-const speak = (message) => {
-  let utterThis = new SpeechSynthesisUtterance();
-  utterThis.text = message;
-  utterThis.volume = 10;
-  utterThis.lang = "fr-FR";
-  console.log(synth.getVoices())
-  utterThis.voice = synth.getVoices()[9];
-  synth.speak(utterThis);
-}
-
-const updateGame = (newCurrentRound) => {
-  let newOrderPlayers = [];
-  newCurrentRound.forEach(el => {
-    let player = {
-      tag: el.unity,
-      spawn: el.spawn,
-      position: gameInstance.getPositionByTag(el.unity)
-    };
-    newOrderPlayers.push(player);
-  });
-  gameInstance.setCurrentTour(newOrderPlayers);
-}
-
-const updateCurrentPlayerText = () => {
-  const ctx = document.getElementById("debug_round").getContext("2d");
-  ctx.font = "30 serif";
-  ctx.clearRect(0, 0, 100, 100);
-  ctx.fillText(gameInstance.getCurrentPlayer().tag, 20, 100);
-}
-
-const drawArrows = (currentPlayer) => {
-  //draw arrows
-  let newCampOrPlot2 = gameInstance.getPlotOrCamp(gameInstance.getCurrentPlayer().position);
-
-  if (currentPlayer.x === 0 && currentPlayer.y === 0)
-    newCampOrPlot2.canvasArrow.drawArrow(gameInstance.getEndArrowsOfPosition(currentPlayer.spawn).endX, gameInstance.getEndArrowsOfPosition(currentPlayer.spawn).endY, gameInstance.getEndArrowsPointsPlot(newCampOrPlot2.possibleDisplacement));
-  else
-    newCampOrPlot2.canvasArrow.drawArrow(currentPlayer.x, currentPlayer.y, gameInstance.getEndArrowsPointsPlot(newCampOrPlot2.possibleDisplacement));
-}
-
-const newRoundEvent = (message) => {
-  let background = document.getElementById("game-container");
-  background.style.backgroundImage = "url('assets/sea.jpg')";
-  let animator = document.querySelectorAll("lottie-player");
-  console.log(animator);
-  animator.forEach((lottie) => {
-    lottie.style.visibility = "hidden";
-  });
-  console.log(animator);
-  audio.src = 'assets/naval.mp3'
-  audio.play();
-
-  let round = message.players;
-  updateGame(round);
-
-  let currentPlayer = gameInstance.getCurrentPlayer();
-  let newCampOrPlot = gameInstance.getPlotOrCamp(currentPlayer.position);
-  newCampOrPlot.highLight(currentPlayer.color);
-
-  updateCurrentPlayerText();
-
-  //alert(JSON.stringify(campOrPlot));
-  newCampOrPlot.enableButton();
-  speak(currentPlayer.faction + ", c'est à votre tour. Jouez le pion " + currentPlayer.tag);
-
-  drawArrows(currentPlayer);
-
-
-}
-
-const onMoveEvent = (message) => {
-  gameInstance.getPlotOrCamp(gameInstance.getCurrentPlayer().position).removeHighlight();
-  gameInstance.removePlayerPlayed();
-  if (gameInstance.currentTour.length > 0) {
-    let currentPlayer = gameInstance.getCurrentPlayer();
-    let newCampOrPlot = gameInstance.getPlotOrCamp(currentPlayer.position);
-    newCampOrPlot.highLight(currentPlayer.color);
-
-    updateCurrentPlayerText();
-    //alert(JSON.stringify(campOrPlot));
-    newCampOrPlot.enableButton();
-    speak(currentPlayer.faction + ", c'est à votre tour. Jouez le pion " + currentPlayer.tag);
-
-    drawArrows(currentPlayer)
-  }
-}
-
-const onFactionSelected = (message) => {
-  message.races.forEach(el => {
-    if (el.available) {
-      return;
-    } else {
-      let race = getRaceValue(el.name);
-      document.getElementById(race + "Text").innerText =
-        "Pris par " + el.username;
-      document.getElementById(race + "Text").className =
-        "text-badge-taken";
-    }
-  });
-}
-
-
-const onWarsStart = () => {
-  let background = document.getElementById("game-container");
-  background.style.backgroundImage = "url('assets/RedSea.jpg')";
-  audio.src = 'assets/sword-music.mp3';
-  audio.play();
-  let animator = document.querySelectorAll("lottie-player");
-  animator.forEach((lottie) => {
-    lottie.style.visibility = "visible";
-    lottie.play();
-  });
-  let message = "Les guerres commencent. Allez miser sur votre application pour conquerir les territoires."
-  speak(message);
-
-}
-
-
-socketClient._client.onmessage = e => {
-  if (e.data !== undefined && JSON.parse(e.data).response !== undefined) {
-    let message = JSON.parse(e.data);
-    console.log(JSON.stringify(message));
-
-    switch (message.response) {
-      case "RACE_SELECTED":
-        onFactionSelected(message)
-        break;
-      case "GAME_START":
-        buildBoard();
-        break;
-      case "NEW_ROUND":
-        newRoundEvent(message);
-        break;
-      case "MOVE":
-        onMoveEvent(message);
-        break;
-      case "START_WARS":
-        onWarsStart();
-        break;
-      default:
-    }
-  }
-};
+socketClient._client.onmessage = e => mySocketHandler(e)
 
 /* App Code */
 const buildApp = () => {
